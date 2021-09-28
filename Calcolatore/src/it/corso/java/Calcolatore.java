201e880ee0e42e7df7a5fc2de6352e8c8f4c8203
Roberto <robertoboero@outlook.it>
2021-09-28 22:43:08 +0200

    Terzo

--- Calcolatore/src/it/corso/java/Calcolatore.java
+++ Calcolatore/src/it/corso/java/Calcolatore.java
@@ -3,6 +3,13 @@ package it.corso.java;
 public class Calcolatore {
 
 	public static void main(String[] args) {
+		Calcolatrice c = new Calcolatrice();
+		double a = 10;
+		double b = 13.5;
+		System.out.println(c.somma(a,b));
+		System.out.println(c.differenza(a,b));
+		System.out.println(c.moltiplicazione(a,b));
+		System.out.println(c.divisione(a,b));
 		
 
 	}
